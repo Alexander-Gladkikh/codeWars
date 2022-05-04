16446a8e94869fd5e09cf406d94393d89893dd2b
Alexander Gladkikh <som17son25@gmail.com>
2022-05-04 20:52:37 +0300

    deleted readme.md create learn.js task1

---
+++ learn.js
@@ -0,0 +1,9 @@
+// 1. Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+  return x.replace(/ /g,'');
+}
+
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
+
+// --------------------------------------------------------------------->

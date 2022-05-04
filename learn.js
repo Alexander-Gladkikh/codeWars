c815d610327e5ee1770890573e944d5b9f929b9c
Alexander Gladkikh <som17son25@gmail.com>
2022-05-04 21:05:24 +0300

    tasks2

--- learn.js
+++ learn.js
@@ -1,9 +1,15 @@
 // 1. Simple, remove the spaces from the string, then return the resultant string.
 
-function noSpace(x){
-  return x.replace(/ /g,'');
+function noSpace(x) {
+  return x.replace(/ /g, "");
 }
 
-console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
+console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
 
 // --------------------------------------------------------------------->
+
+// 2. Given an array of integers, return a new array with each value doubled.
+
+function maps(x) {
+  return x.map((elem) => elem * 2);
+}

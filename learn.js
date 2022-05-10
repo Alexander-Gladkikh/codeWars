0d25c995d7e351e63938ccfc9593ed830427d314
Alexander Gladkikh <som17son25@gmail.com>
2022-05-10 13:44:13 +0300

    task9correction

--- learn.js
+++ learn.js
@@ -140,7 +140,7 @@ console.log(countSheeps(array1));
 For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. */
 
 function squareDigits(num){
-  return String(num).split('').map((elem) => Number(elem)**2).join("");
+  return Number(String(num).split('').map((elem) => Number(elem)**2).join(""));
 }
 
 console.log(squareDigits(3212));

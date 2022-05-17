5384ae6e57ccc878fa1b91d5edab4d33ed93e049
Alexander Gladkikh <som17son25@gmail.com>
2022-05-17 17:03:04 +0300

    task 11

--- learn.js
+++ learn.js
@@ -199,3 +199,31 @@ function DNAStrand(dna){
 console.log(DNAStrand("AAAAA"));
 
 //------------------------------------------------------------------>
+
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples:
+
+// rps('scissors','paper') // Player 1 won!
+// rps('scissors','rock') // Player 2 won!
+// rps('paper','paper') // Drawn
+
+const rps = (p1, p2) => {
+  if(p1 == p2) {
+    return "Draw!"
+  }
+  else if ((p1 == 'scissors' && p2 == 'paper') || (p1 == "rock" && p2 == "scissors") || (p1 == "paper" && p2 == "rock")){
+    return "Player 1 won!";
+  }
+   else {
+     return "Player 2 won!"
+   }
+};
+
+
+const rps2 = (p1, p2) => {
+  if(p1 === p2){
+    return "Drown"
+  }
+  return `Player ${/scissorspaper|paperrock|rockscissors/.test(p1+p2)? 1:2} win`
+}
